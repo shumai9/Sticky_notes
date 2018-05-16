769262469e11b4defb73a332c4cda47a1edfc330
Shumai <shumai9@github.com>
2018-05-16 20:05:11 +0100

    added button fnctionality for note add and update

---
+++ src/Board.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import Note from './Note'
+import FaPlus from 'react-icons/lib/fa/plus'
+
+
+class Board extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { notes:[ {id:0, note:" hey "} ]}
+        this.eachNote= this.eachNote.bind(this)
+        this.update = this.update.bind(this)
+        this.remove = this.remove.bind(this)
+        this.add = this.add.bind(this)
+        this.nextId = this.nextId.bind(this)
+    }
+
+    update(newText, i){
+        
+        this.setState(prevState => ({
+            notes: prevState.notes.map( note =>(note.id !== i) ? note : { ...note, note: newText} )
+        }))
+    }
+
+    add(text) {
+        this.setState( prevState => ({
+            notes: [ 
+                ...prevState.notes,
+                {
+                    id:this.nextId,
+                    note: text
+                }
+            ]
+        }))
+    }
+//notes: [...prevState.notes, {id:(this.notes.length + 1), note:( text)}
+    nextId() {
+        this.uniqueId = this.uniqueId || 0
+        return this.uniqueId++
+    }
+
+    remove(id){
+        console.log("removing item with id", id)
+        this.setState( prevState => ({
+            notes: prevState.notes.filter(note => note.id !== id)
+        }))
+    }
+
+    eachNote(note, i) {
+        return (
+            <Note key={i}
+                index={i}
+                onChange={this.update}
+                onRemove={this.remove}>
+                {note.note}
+             </Note>
+        )
+    }
+
+    render() {
+        return(
+            <div className="board">
+                {this.state.notes.map(this.eachNote)}
+                    <button id="add" onClick={this.add.bind(null, "input")}>
+                        <FaPlus />
+                    </button>
+                </div>
+            )
+        }
+    }
+export default Board
